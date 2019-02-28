c4dfc38af16ee29158ce221d6c19a071c2d7cf02
NIghtwarri0R <gamena750@abv.bg>
2019-02-28 20:29:27 +0000

    I give up
    Here is whatever i have got so far

--- loadingFile.cpp
+++ loadingFile.cpp
@@ -1,1 +1,40 @@
-import
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+
+string loadFile();
+using namespace std; 
+
+ *myFile; 
+
+size_t 
+
+int main(){
+
+vector<string> loadedString; 
+
+}
+
+string loadFile(){
+
+
+myFile->open("b_lovely_landscapes.txt");
+
+while(myFile->readLine()){
+
+	myFile->std::vector<char> v;
+	if (myFile == fopen("b_lovely_landscapes.txt", "r"))
+	{
+		char buf[1024];
+		
+	}
+}
+
+
+
+
+}
+
+
